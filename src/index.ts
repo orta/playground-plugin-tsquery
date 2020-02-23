6fef689bbc5677117248a1725c0d1b7213a9066d
Orta Therox <orta.therox@gmail.com>
2020-02-23 09:05:08 -0500

    WIP on rendering AST

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,123 @@
-import  { tsquery } from "@phenomnomnominal/tsquery"
+import { tsquery } from "@phenomnomnominal/tsquery";
+import { el, renderAST } from "./vendor/utils";
+import type { editor } from "monaco-editor";
+import  type { Node } from "typescript";
 
-const customPlugin: import('./vendor/playground').PlaygroundPlugin = {  
-  id: 'tsquery',
-  displayName: 'TSQuery',
+let astVersion = -1
+let ast:Node = undefined
+
+const customPlugin: import("./vendor/playground").PlaygroundPlugin = {
+  id: "tsquery",
+  displayName: "TSQuery",
+  // data: {
+
+  // },
   didMount: (sandbox, container) => {
     // @ts-ignore
-    window.tsquery = tsquery
+    window.tsquery = tsquery;
 
-    console.log('Showing new plugin')
+    const p = (str: string) => el(str, "p", container);
+    const h4 = (str: string) => el(str, "h4", container);
 
-    const p = document.createElement('p')
-    p.textContent = 'Playground plugin defaults'
-    container.appendChild(p)
+    h4("Using TSQuery");
+    p(`Create a gist with an <code>index.md</code> using a set of <code>---</code>s to split between slides. You can find out more about the syntax <a href="https://github.com/orta/playground-slides">here</a>. If you want to demo the slides, click here to try <a href="#" onclick='document.getElementById("gist-input").value = "https://gist.github.com/orta/d7dbd4cdb8d1f99c52871fb15db620bc"'>an existing deck</a>.`);
 
-    const startButton = document.createElement('input')
-    startButton.type = 'button'
-    startButton.value = 'Change the code in the editor'
-    container.appendChild(startButton)
+    const gistForm = createGistInputForm(sandbox);
+    container.appendChild(gistForm);
 
-    startButton.onclick = () => {
-      sandbox.setText('You clicked the button!')
-    }
+    const results = document.createElement("div");
+    results.id = "query-results";
+    container.appendChild(results);
+
+    const model = sandbox.editor.getModel()
+    runQuery(sandbox, model)
   },
 
-  modelChangedDebounce: async (sandbox, _model) => {
-    const ast = await sandbox.getAST()
-    console.log(tsquery(ast, "Identifier"))
+  modelChangedDebounce: async (sandbox, model) => {
+    
+    runQuery(sandbox, model)
   },
 
   didUnmount: () => {
-    console.log('Removing plugin')
-  },
+    console.log("Removing plugin");
+  }
+};
+
+const runQuery = async (sandbox, model: editor.ITextModel) => {
+  const query = localStorage.getItem("playground-tsquery-current-query");
+  if (!query) return;
+
+  console.log(query)
+
+  // Cleanup
+  const results = document.getElementById("query-results");
+  if (!results) return;
+  while (results.firstChild) {
+    results.removeChild(results.firstChild);
+  }
+  
+  if (model.getVersionId() !== astVersion) {
+    ast = await sandbox.getAST();
+    astVersion = model.getVersionId()
+  }
+
+  const queryResults = tsquery(ast, query);
+  
+  console.log(queryResults);
+  queryResults.forEach(node => {
+    // @ts-ignore
+    renderAST(results, node);
+  });
 }
 
-export default customPlugin
+const createGistInputForm = (sandbox) => {
+  const form = document.createElement("form");
+
+  const gistHref = document.createElement("input");
+  gistHref.id = "tsquery-input";
+  gistHref.placeholder = `Identifier[name="Animal"]`;
+  gistHref.autocomplete="off" 
+  gistHref.autocapitalize="off" 
+  gistHref.spellcheck=false
+  // @ts-ignore
+  gistHref.autocorrect="off" 
+
+  const storedGistHref = localStorage.getItem("playground-tsquery-current-query");
+  gistHref.value = storedGistHref;
+
+  const updateState = ({ enable }) => {
+    if (enable) {
+      gistHref.classList.add("good");
+      // startButton.disabled = false
+    } else {
+      gistHref.classList.remove("good");
+      // startButton.disabled = true
+    }
+  };
+
+  const textUpdate = e => {
+    const href = e.target.value.trim();
+    localStorage.setItem("playground-tsquery-current-query", href);
+    
+    console.log("text")
+    const model = sandbox.editor.getModel()
+    runQuery(sandbox, model)
+
+    // updateState({ enable: isGist(href) })
+  };
+
+  
+  gistHref.addEventListener("input", textUpdate)
+
+  // gistHref.onkeyup = textUpdate;
+  // gistHref.onpaste = textUpdate;
+  // gistHref.onchange = textUpdate;
+  // gistHref.onblur = textUpdate;
+  // gistHref.oninput = textUpdate;
+  form.appendChild(gistHref);
+
+  updateState({ enable: gistHref.textContent && gistHref.textContent.length });
+  return form;
+};
+
+export default customPlugin;
--- src/vendor/utils.ts
+++ src/vendor/utils.ts
@@ -1,14 +1,69 @@
+import type { Node } from "typescript"
+
 /** Get a relative URL for something in your dist folder depending on if you're in dev mode or not */
 export const requireURL = (path: string) => {
   // https://unpkg.com/browse/typescript-playground-presentation-mode@0.0.1/dist/x.js => unpkg/browse/typescript-playground-presentation-mode@0.0.1/dist/x
-  const isDev = document.location.host.includes('localhost')
-  const prefix = isDev ? 'local/' : 'unpkg/typescript-playground-presentation-mode/dist/'
-  return prefix + path
-}
+  const isDev = document.location.host.includes("localhost");
+  const prefix = isDev ? "local/" : "unpkg/typescript-playground-presentation-mode/dist/";
+  return prefix + path;
+};
 
 /** Use this to make a few dumb element generation funcs */
 export const el = (str: string, el: string, container: Element) => {
-  const para = document.createElement(el)
-  para.innerHTML = str
-  container.appendChild(para)
-}
+  const para = document.createElement(el);
+  para.innerHTML = str;
+  container.appendChild(para);
+};
+
+/** Renderers a TypeScript AST */
+export const renderAST = (parent: HTMLElement, node: Node) => {
+  const renderItem = (parentElement: Element, node: Node) => {
+    const ul =  document.createElement("ul")
+    ul.className = "ast-tree"
+
+    const li = document.createElement("li")
+    ul.appendChild(li)
+    
+    const a = document.createElement("a")
+    a.textContent = String(node.kind)
+    li.appendChild(a)
+
+    const kids = node.getChildren()
+    if (kids.length) {
+      const childUl =  document.createElement("ul")
+      childUl.className = "ast-tree"
+      li.appendChild(childUl)
+  
+      for (const child of kids) {
+        renderItem(childUl, child)
+      } 
+  
+      parentElement.appendChild(ul)
+    }
+  }
+
+  renderItem(parent, node)
+
+
+  const tree = document.querySelectorAll("ul.ast-tree a:not(:last-child)");
+  for (var i = 0; i < tree.length; i++) {
+    tree[i].addEventListener("click", function(e: MouseEvent) {
+
+      // @ts-ignore
+      const parent = e.target.parentElement;
+      const classList = parent.classList;
+      
+      if (classList.contains("open")) {
+        classList.remove("open");
+        var opensubs = parent.querySelectorAll(":scope .open");
+
+        for (var i = 0; i < opensubs.length; i++) {
+          opensubs[i].classList.remove("open");
+        }
+      } else {
+        classList.add("open");
+      }
+      e.preventDefault();
+    });
+  }
+};
